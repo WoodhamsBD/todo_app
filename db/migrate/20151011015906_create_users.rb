4bf1ed234b8912ed4c25df95bebe0e75d941309e
Bryan Woodhams <Woodhamsbd@gmail.com>
2016-03-24 16:30:55 -0700

    User Migration cleanup

--- db/migrate/20151011015906_create_users.rb
+++ db/migrate/20151011015906_create_users.rb
@@ -5,6 +5,12 @@ class CreateUsers < ActiveRecord::Migration
       t.string :email
       t.string :password_digest
       t.string :remember_digest
+      t.string :activation_digest
+      t.boolean :acivated, default: false
+      t.datetime :activated_at
+      t.string :reset_digest
+      t.datetime :reset_sent_at
+      t.boolean :admin, default: false
 
       t.timestamps null: false
     end
